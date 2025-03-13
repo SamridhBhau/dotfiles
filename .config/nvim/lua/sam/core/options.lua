f31c4a8b752a494ae034239c833f9e69b336c490
Samridh <sb.samridhbhau@gmail.com>
2025-03-13 23:44:11 +0530

    nvim

---
+++ .config/nvim/lua/sam/core/options.lua
@@ -0,0 +1,20 @@
+local o = vim.o
+
+o.rnu = true
+o.number = true
+
+o.autoindent = true
+o.expandtab = true
+o.tabstop = 2
+o.shiftwidth = 2
+o.scrolloff = 6
+
+-- search
+o.hls = false
+o.ignorecase = true
+o.smartcase = true
+
+o.cursorline = true
+o.signcolumn = "yes"
+
+vim.g.mapleader = " "
