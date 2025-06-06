662c462fc63e847a6f03f6c0e771cd98b8c37f5b
samridh <sb.samridhbhau@gmail.com>
2025-06-06 14:35:31 +0530

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
-export NNN_BMS="c:$HOME/Documents/programs/;b:$HOME/Documents/Books/;s:$HOME/Documents/Study Materials/SEM-VI/;d:$HOME/Documents/;D:$HOME/Downloads/"
-
 # Start X at login
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
         exec startx -- -keeptty
     end
 end
+fish_vi_key_bindings
+export NNN_BMS="c:$HOME/Documents/programs/;b:$HOME/Documents/Books/;s:$HOME/Documents/Study Materials/SEM-VI/;d:$HOME/Documents/;D:$HOME/Downloads/"
